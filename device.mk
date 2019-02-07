872aa5343f9be3ec703341a25395a1545a234eae
Matthias Lenk <matthias.lenk@gmx.de>
2019-02-07 20:42:31 +0100

    lavender: device: Replace AOSP USB HAL with Lineage basic USB HAL
    Using the AOSP HAL, changing USB modes (such as MTP when connecting to
    PC) does not work through the "USB Preferences" settings fragment (but
    it does through Developer Options.
    
    Using the Lineage basic HAL, MTP mode works properly. The HAL also
    reports canChangeDataRole = false and canChangePowerRole = false.
    
    Change-Id: I52716a04e65ba446c9412dd3328df407d02d107a
    Signed-off-by: Matthias Lenk <matthias.lenk@gmx.de>
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>
    Signed-off-by: Sebita <kjjjnob.seba@gmail.com>

--- device.mk
+++ device.mk
@@ -457,7 +457,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.xiaomi_lavender
+    android.hardware.usb@1.0-service.basic
 
 # Vibrator
 PRODUCT_PACKAGES += \
