74fe7d9e8ac806a0568e7e119afcad6b9acf9346
DarkDroidDev <darkdroid.dev@gmail.com>
2019-07-02 03:10:05 +0200

    lavender: add TARGET_VENDOR

--- aosp_lavender.mk
+++ aosp_lavender.mk
@@ -23,6 +23,8 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_VENDOR := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="lavender" \
@@ -30,11 +32,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="lavender-user 10 QP1A.191005.007 V10.3.6.0.PFGMIXM release-keys"
 
 BUILD_FINGERPRINT := "google/blueline/blueline:10/QP1A.191105.003/5899767:user/release-keys"
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=$(BUILD_FINGERPRINT)
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
